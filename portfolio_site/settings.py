67d08d974cd095637720685e6e504db9fcfe8a63
Dominic62q <dominicquainoo62@gmail.com>
2026-01-16 13:31:51 +0000

    Configure media files to be served via WhiteNoise in production

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -184,3 +184,8 @@ if not DEBUG:
 # Media files (uploads)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# In production, media files are copied to static during build
+# This allows WhiteNoise to serve them
+if not DEBUG:
+    MEDIA_URL = '/static/media/'
